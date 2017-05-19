2d088779cd47c7f8e3b9a02477b6e8822ba53386
Quentin M <quent-200193@hotmail.fr>
2017-05-19 22:14:59 +0000

    sphere normalization script

---
+++ PythonFile/Sphere.py
@@ -0,0 +1,53 @@
+import numpy as np
+import numpy.matlib
+import tools
+import math
+import struct
+import array
+import time
+
+__author__="Quentin MASCRET <quentin.mascret.1@ulaval.ca>"
+__date__="2017-04-14"
+__version__="1.1-dev"
+
+class Sphere_calibration(object):
+	def __init__(self):
+		self.__rayon=1
+		self.__center=np.array([0,0,0])
+	
+	def Length(self, features):
+		self.__DataLength=features.shape[0]
+		self.__feature=np.zeros((39,features.shape[0]*150))
+		self.__Output=np.zeros(features.shape[0]*1950) # 13 * 150
+		#return DataLength , feature, Output
+		
+	def ClassAndFeaturesSplit(self, features, label):
+		self.Length(features)
+		for i in range(0,self.__DataLength):
+			idx=(i*150+np.arange(150))
+			idx_2=(i*1950+np.arange(1950))
+			self.__feature[:,idx]=np.reshape(features,(39,150), order='F')
+			self.__Output[idx_2]=numpy.matlib.repmat(label,1,1950)
+		# reshape all matrix in 3 vector -> MFCC, VMFCC, VVMFCC
+		x=self.__feature[(0+np.arange(13)),:].reshape(self.__feature[(0+np.arange(13)),:].size, order='F')
+		y=self.__feature[(13+np.arange(13)),:].reshape(self.__feature[(13+np.arange(13)),:].size, order='F')
+		z=self.__feature[(26+np.arange(13)),:].reshape(self.__feature[(26+np.arange(13)),:].size, order='F')
+		
+		# mean and std of x, y and z
+		meanx=np.mean(x)
+		meany=np.mean(y)
+		meanz=np.mean(z)
+		stdx=np.std(x)
+		stdy=np.std(y)
+		stdz=np.std(z)
+		
+		# 0 mean and one std
+		
+		
+
+if __name__ == "__main__" :
+	Sphere= Sphere_calibration()
+	b=np.ones(5850)
+	b[1]=2
+	b[5823]=18
+	Sphere.ClassAndFeaturesSplit(b,1)
